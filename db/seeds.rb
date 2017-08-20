938abd300fcc0c487492b59fab58e12a788455d8
gabidavila <gabidavila@gmail.com>
2017-08-19 22:39:16 -0400

    associations, schema, seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Creating Authors
+
+author_1 = Author.create(name: "Gabriela F.")
+author_2 = Author.create(name: "Martina W")
+author_3 = Author.create(name: "Julia D.")
+
+# Creating Categories
+
+category_1 = Category.create(name: "Sports")
+category_2 = Category.create(name: "Politics")
+category_3 = Category.create(name: "Economy")
+category_4 = Category.create(name: "Entertainment")
+category_5 = Category.create(name: "Social")
+
+# Creating Posts
+
+content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis ligula justo. Ut a lectus ac risus vulputate dapibus. Nulla eget purus varius, faucibus arcu vitae, pulvinar mi. Ut maximus non nulla id vulputate. In at varius nulla, in accumsan lacus. Fusce et ligula pellentesque, sagittis eros ut, sollicitudin felis. Aenean dapibus lorem nibh, eget ornare ante venenatis ut. Nunc finibus velit mi, ac semper elit faucibus et. In a cursus urna. Etiam orci tellus, commodo at consequat quis, convallis sed metus. In eu turpis purus. Integer vitae lobortis ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam semper mattis arcu et viverra. Etiam consequat vestibulum mauris quis lobortis. Nam iaculis quis lectus eget aliquam."
+
+post_1 = Post.create(title: "USA Soccer Team wins olympics", content: content, categories: [category_1, category_5], author: author_2)
+post_2 = Post.create(title: "Another White House Scandal", content: content, categories: [category_2, category_5], author: author_3)
+post_3 = Post.create(title: "The Pound Sterlink Tanked after BREXIT", content: content, categories: [category_2, category_3], author: author_1)
