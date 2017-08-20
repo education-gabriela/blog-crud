274879716d6e50bc19403f6639834b74dd9ce924
gabidavila <gabidavila@gmail.com>
2017-08-19 22:59:18 -0400

    listing categories and authors

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   belongs_to :author
+
+  accepts_nested_attributes_for :categories
 end
